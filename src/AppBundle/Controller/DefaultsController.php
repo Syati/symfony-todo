da244a1b614f41327dee7d1332f1fe0711aef072
mizuki-y <mizuki-y@syati.info>
2017-10-22 14:53:48 +0900

    Rename base.html to layout.html
    - rename default con to defaults

--- src/AppBundle/Controller/DefaultsController.php
+++ src/AppBundle/Controller/DefaultsController.php
@@ -7,11 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * DefaultController
+ * DefaultsController
  *
  * @author mizuki-y <mizuki-y@syati.info>
  */
-class DefaultController extends Controller
+class DefaultsController extends Controller
 {
     /**
      * Default indexAction renders a index view
@@ -26,7 +26,7 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render(
-            'default/index.html.twig',
+            'Defaults/index.html.twig',
             array(
                 'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..')
             )
